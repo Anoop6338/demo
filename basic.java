efa28ac6ef40eced98eb81e217360ac0488db996
Anoop Kumar <kumaranoop6338@gmail.com>
2023-02-05 23:25:53 +0530

    comments deleted

--- basic.java
+++ basic.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class basic {
     public static void printLetters(String str){
         for(int i=0; i<str.length(); i++){
@@ -8,24 +6,7 @@ public class basic {
         System.out.println();
     }
     public static void main(String[] args) {
-    /* 
-        //invoke methods
-        String str = "abcd";    //Method 1
-        String str2 = new String("xyz");    //Mathod 2
-        //there is diference in these two methods which we will discuss later
-        
-        //Strings are IMMUTABLE
-
-        Scanner sc = new Scanner(System.in);
-        //String name = sc.next();    // just take input till space
-
-        String name = sc.nextLine();    //takes whole line as input
-
-        System.out.println(name);
-
-        System.out.println(name.length());  // prints length of string
-
-    */
+  
 
         //Concatination
         String firstname = "Anoop";
